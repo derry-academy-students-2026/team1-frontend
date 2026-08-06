6686f44131be564e18bf4fb0a942e6d165df9654
samwatson36 <samwatson36@protonmail.com>
2026-08-06 14:50:53 +0100

    US038: Adding biome linting checks to the frontend. (#2)

--- src/controllers/jobController.ts
+++ src/controllers/jobController.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
 
-export function getHome(req: Request, res: Response) {
-  res.send("<!doctype html><html><body><h1>Hello world</h1></body></html>");
+export function getHome(_req: Request, res: Response) {
+	res.send("<!doctype html><html><body><h1>Hello world</h1></body></html>");
 }
