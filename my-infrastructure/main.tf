4e31b6fabad64b41835dce75bd4cdcce87af356b
chloe-mcc <chloe.mccarter@kainos.com>
2026-08-26 14:01:15 +0100

    Make Terraform pipeline ready frontend (#22)
    * Make Terraform pipeline ready frontend
    
    * Terraform deployment fixes
    
    * Added comments and linting fixes

--- my-infrastructure/main.tf
+++ my-infrastructure/main.tf
@@ -1,9 +1,11 @@
 terraform {
+  # key is intentionally omitted here and supplied per-environment via
+  # `terraform init -backend-config="key=team1-frontend-<env>.terraform.tfstate"`
+  # so each environment (dev, prod, ...) gets its own state file.
   backend "azurerm" {
     resource_group_name  = "team1-tfstate-rg"
     storage_account_name = "team1tfstatecm20260825"
     container_name       = "tfstate"
-    key                  = "team1-frontend.terraform.tfstate"
     use_azuread_auth     = true
     use_cli              = true
   }
@@ -20,10 +22,16 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  # dev, prod, etc. all share this naming convention - adding an
+  # environments/prod.tfvars file is all that's needed for a prod environment
+  resource_group_name = "${var.resource_group_name}-${var.environment}-rg"
+}
+
 module "resource_group" {
   source = "./modules/resource-group"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = local.resource_group_name
   location            = var.location
 
   tags = {
